742d11523f4179931d46c581f8d037a9e40ebcf5
MilashaThathsarani <milashathathsarni99@gmail.com>
2021-12-25 22:15:55 +0530

    Rename LoginForm.fxml

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -13,7 +13,7 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/loginForm.fxml"))));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/LoginForm.fxml"))));
         primaryStage.show();
     }
 }
