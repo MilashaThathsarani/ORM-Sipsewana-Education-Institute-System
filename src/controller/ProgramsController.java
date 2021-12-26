f369efe83cde5ee884ed0520531c548c6a3674db
MilashaThathsarani <milashathathsarni99@gmail.com>
2021-12-26 21:23:12 +0530

    updated content

--- src/controller/ProgramsController.java
+++ src/controller/ProgramsController.java
@@ -1,4 +1,49 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ProgramsController {
+    public Label txtDate;
+    public Label txtTime;
+    public JFXButton btnBack;
+    public TextField txtProgramName;
+    public JFXTextField txtId;
+    public JFXTextField txtSearch;
+    public TextField txtDuration;
+    public TextField txtFee;
+    public TableView tblProgram;
+    public TableColumn colProgramId;
+    public TableColumn colProgramName;
+    public TableColumn colDuration;
+    public TableColumn colFee;
+    public JFXButton btnAdd;
+    public JFXButton btnUpdate;
+    public JFXButton btnDelete;
+
+    public void backOnAction(ActionEvent actionEvent) {
+    }
+
+    public void programNameOnAction(ActionEvent actionEvent) {
+    }
+
+    public void durationOnAction(ActionEvent actionEvent) {
+    }
+
+    public void feeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/controller/StudentRegistrationController.java
+++ src/controller/StudentRegistrationController.java
@@ -1,4 +1,69 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class StudentRegistrationController {
+    public Label txtDate;
+    public Label txtTime;
+    public JFXButton btnBack;
+    public TextField txtFullName;
+    public TextField txtAddress;
+    public DatePicker dpBirthday;
+    public TextField txtAge;
+    public TextField txtPhoneNumber;
+    public TextField txtxEducation;
+    public JFXButton btnAdd;
+    public JFXButton btnUpdate;
+    public JFXButton btnDelete;
+    public TableView tblStudent;
+    public TableColumn colStudentId;
+    public TableColumn colFullName;
+    public TableColumn colAddress;
+    public TableColumn coBirthday;
+    public TableColumn colAge;
+    public TableColumn colGender;
+    public TableColumn colPhoneNumber;
+    public TableColumn colEducation;
+    public ComboBox cmbGender;
+    public JFXTextField txtId;
+    public JFXTextField txtSearch;
+
+    public void backOnAction(ActionEvent actionEvent) {
+    }
+
+    public void fullNameOnAction(ActionEvent actionEvent) {
+        txtAddress.requestFocus();
+    }
+
+    public void addressOnAction(ActionEvent actionEvent) {
+        dpBirthday.requestFocus();
+    }
+
+    public void birthdayOnAction(ActionEvent actionEvent) {
+        txtAge.requestFocus();
+    }
+
+    public void ageOnAction(ActionEvent actionEvent) {
+        cmbGender.requestFocus();
+    }
+
+    public void phoneNumberOnAction(ActionEvent actionEvent) {
+        txtxEducation.requestFocus();
+    }
+
+    public void genderOnaction(ActionEvent actionEvent) {
+        txtPhoneNumber.requestFocus();
+    }
+
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
 }
