41096ed0cca362fa0bf21df8536c76a316b19ead
MilashaThathsarani <milashathathsarni99@gmail.com>
2021-12-27 13:02:02 +0530

    Content updated

---
+++ src/entity/Program.java
@@ -0,0 +1,75 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    private String programId;
+    private String programName;
+    private String duration;
+    private double fee;
+
+    private List<Student> studentList = new ArrayList<>();
+
+    public Program() {
+    }
+
+    public Program(String programId, String programName, String duration, double fee, List<Student> studentList) {
+        this.setProgramId(programId);
+        this.setProgramName(programName);
+        this.setDuration(duration);
+        this.setFee(fee);
+        this.setStudentList(studentList);
+    }
+
+    public String getProgramId() {
+        return programId;
+    }
+
+    public void setProgramId(String programId) {
+        this.programId = programId;
+    }
+
+    public String getProgramName() {
+        return programName;
+    }
+
+    public void setProgramName(String programName) {
+        this.programName = programName;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    @Override
+    public String toString() {
+        return "Program{" +
+                "programId='" + programId + '\'' +
+                ", programName='" + programName + '\'' +
+                ", duration='" + duration + '\'' +
+                ", fee=" + fee +
+                ", studentList=" + studentList +
+                '}';
+    }
+}
---
+++ src/entity/Student.java
@@ -0,0 +1,119 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String studentId;
+    private String studentName;
+    private String address;
+    private String birthday;
+    private int age;
+    private String gender;
+    private String phoneNumber;
+    private String education;
+
+    private List<Program> programList = new ArrayList<>();
+
+    public Student() {
+    }
+
+    public Student(String studentId, String studentName, String address, String birthday, int age, String gender, String phoneNumber, String education, List<Program> programList) {
+        this.setStudentId(studentId);
+        this.setStudentName(studentName);
+        this.setAddress(address);
+        this.setBirthday(birthday);
+        this.setAge(age);
+        this.setGender(gender);
+        this.setPhoneNumber(phoneNumber);
+        this.setEducation(education);
+        this.setProgramList(programList);
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public List<Program> getProgramList() {
+        return programList;
+    }
+
+    public void setProgramList(List<Program> programList) {
+        this.programList = programList;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId='" + studentId + '\'' +
+                ", studentName='" + studentName + '\'' +
+                ", address='" + address + '\'' +
+                ", birthday='" + birthday + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", education='" + education + '\'' +
+                ", programList=" + programList +
+                '}';
+    }
+}
