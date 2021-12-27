c9e356eb23d52176d3cfa26102748576a5ca23c5
MilashaThathsarani <milashathathsarni99@gmail.com>
2021-12-27 16:33:38 +0530

    Content updated

--- src/util/FactoryConfiguration.java
+++ src/util/FactoryConfiguration.java
@@ -1,2 +1,38 @@
+package util;
+
+import entity.Program;
+import entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration(){
+        Properties properties = new Properties();
+        try {
+            properties.load(ClassLoader.getSystemClassLoader().getResourceAsStream("hibernate.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException("There is issue in Hibernate util");
+        }
+            Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class)
+                    .addAnnotatedClass(Program.class).mergeProperties(properties);
+            sessionFactory = configuration.buildSessionFactory();
+        }
+
+        public static FactoryConfiguration getInstance() {
+            return (factoryConfiguration == null) ? factoryConfiguration = new FactoryConfiguration()
+                    : factoryConfiguration;
+        }
+        public Session getSession() {
+
+            return sessionFactory.openSession();
+        }
+    }
 
 
