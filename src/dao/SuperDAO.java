ea2175a45295b1e92096fe5a3902c59c5f490a13
MilashaThathsarani <milashathathsarni99@gmail.com>
2021-12-28 13:05:07 +0530

    Content updated

--- src/dao/SuperDAO.java
+++ src/dao/SuperDAO.java
@@ -1,4 +1,19 @@
 package dao;
 
-public interface SuperDAO {
+
+import entity.SuperEntity;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface SuperDAO<Entity extends SuperEntity, ID> {
+    boolean add(Entity entity) throws SQLException, ClassNotFoundException;
+
+    boolean delete(ID id) throws SQLException, ClassNotFoundException;
+
+    boolean update(Entity entity) throws SQLException, ClassNotFoundException;
+
+    Entity search(ID id) throws SQLException, ClassNotFoundException;
+
+    ArrayList<Entity> getAll() throws SQLException, ClassNotFoundException;
 }
--- src/dao/custom/StudentDAO.java
+++ src/dao/custom/StudentDAO.java
@@ -1,6 +1,5 @@
 package dao.custom;
 
-import dao.CrudDAO;
 import entity.Student;
 
 public interface StudentDAO extends CrudDAO<Student,String> {
